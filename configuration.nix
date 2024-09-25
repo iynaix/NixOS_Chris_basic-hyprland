54dccf841ee86e34421accdff7a4ac89b82ca2f0
KrissBZH <58766203+KrissBZH@users.noreply.github.com>
2024-09-25 04:49:46 +0200

    Add files via upload

---
+++ configuration.nix
@@ -0,0 +1,193 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+      
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+fonts.packages = with pkgs; [
+  roboto
+  nerdfonts
+  fira-code-nerdfont
+  font-awesome
+];
+
+# Mes alias 
+programs.bash.shellAliases = { 
+    nixup = "sudo nixos-rebuild switch --upgrade";
+    cleans = "sudo nix-store --gc";
+    cleang = "sudo nix-collect-garbage -d";
+    nixed = "sudo nano /etc/nixos/configuration.nix";
+    nrb = "sudo nixos-rebuild switch";
+    };
+
+# Enable the X11 windowing system.
+#  services.xserver.enable = true;
+
+services.greetd = {
+  enable = true;
+  settings = rec {
+    initial_session = {
+      command = "Hyprland";
+      user = "chris";
+    };
+    default_session = initial_session;
+  };
+};
+
+    security.rtkit.enable = true;
+    services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+  };
+
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "ChrisNIX"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+# Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
+  
+  hardware.nvidia.modesetting.enable = true;
+  hardware.nvidia.powerManagement.enable = true;
+  hardware.nvidia.powerManagement.finegrained = false;
+  hardware.nvidia.open = false;
+  hardware.nvidia.nvidiaSettings = true;
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.beta;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Paris";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "fr_FR.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "fr_FR.UTF-8";
+    LC_IDENTIFICATION = "fr_FR.UTF-8";
+    LC_MEASUREMENT = "fr_FR.UTF-8";
+    LC_MONETARY = "fr_FR.UTF-8";
+    LC_NAME = "fr_FR.UTF-8";
+    LC_NUMERIC = "fr_FR.UTF-8";
+    LC_PAPER = "fr_FR.UTF-8";
+    LC_TELEPHONE = "fr_FR.UTF-8";
+    LC_TIME = "fr_FR.UTF-8";
+  };
+
+  # Configure console keymap
+  console.keyMap = "fr";
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.chris = {
+    isNormalUser = true;
+    description = "Chris";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+
+  # Enable automatic login for the user.
+  services.getty.autologinUser = "chris";
+
+  # hyprland
+	programs.hyprland.enable = true;
+	xdg.portal = {
+	enable = true;
+	wlr.enable = true;
+	xdgOpenUsePortal = true;
+	extraPortals = [
+	pkgs.xdg-desktop-portal-hyprland
+	pkgs.xdg-desktop-portal-gtk
+	];
+	};
+
+	# mes softs
+	programs.firefox.enable = true;
+	programs.thunar.enable = true;
+	programs.waybar.enable = true;
+	programs.file-roller.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+   xfce.ristretto
+   adwaita-icon-theme-legacy
+   tela-circle-icon-theme
+   webp-pixbuf-loader
+   libwebp
+   xarchiver
+   hyprpaper
+   slurp
+   alacritty
+   xfce.mousepad
+   wofi
+   util-linux
+   pciutils
+   ntfs3g
+   gimp
+   inkscape
+   libreoffice-fresh
+   dmenu-wayland
+   gvfs
+   xfce.tumbler
+   fastfetch
+   grim
+   dunst
+   networkmanagerapplet
+   pavucontrol
+   nwg-look
+   ];
+
+  services.gvfs.enable = true;
+  services.tumbler.enable = true;  
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+}
